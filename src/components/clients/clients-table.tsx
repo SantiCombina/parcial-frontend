f16f6d0a0de6e0e927d330fdf7d3fe99b1af2900
SantiCombina <santicombina@gmail.com>
2024-06-25 21:22:49 -0300

    Update ClientsTable component to use modals for client actions

--- src/components/clients/clients-table.tsx
+++ src/components/clients/clients-table.tsx
@@ -1,8 +1,15 @@
 import {ColumnDef} from "@tanstack/react-table";
 
 import {DataTable} from "./data-table";
+import {DeleteClientModal} from "./modals/delete-client-modal";
+import {UpdateClientModal} from "./modals/update-client-modal";
 
-export function ClientsTable({clientes}: {clientes: Cliente[] | undefined}) {
+interface Props {
+    refetch: () => void;
+    clientes: Cliente[] | undefined;
+}
+
+export function ClientsTable({clientes, refetch}: Props) {
     const columns: ColumnDef<Cliente>[] = [
         {
             accessorKey: "nombre",
@@ -24,39 +31,21 @@ export function ClientsTable({clientes}: {clientes: Cliente[] | undefined}) {
             accessorKey: "saldo",
             header: "Saldo",
         },
+        {
+            accessorKey: "actions",
+            header: "Acciones",
+            cell: ({row}) => {
+                const items = row.original;
+
+                return (
+                    <div className="flex items-center gap-2">
+                        <UpdateClientModal cliente={items} refetch={refetch} />
+                        <DeleteClientModal cliente={items} refetch={refetch} />
+                    </div>
+                );
+            },
+        },
     ];
 
-    return (
-        <DataTable columns={columns} data={clientes!} />
-        // <Table>
-        //     <TableHeader>
-        //         <TableRow>
-        //             <TableHead className="w-[100px]">Nombre</TableHead>
-        //             <TableHead>Domicilio</TableHead>
-        //             <TableHead>Localidad</TableHead>
-        //             <TableHead className="text-right">Promotor</TableHead>
-        //             <TableHead className="text-right">Saldo</TableHead>
-        //         </TableRow>
-        //     </TableHeader>
-        //     <TableBody>
-        //         {clientes?.map((cliente) => (
-        //             <TableRow key={cliente.id}>
-        //                 <TableCell className="font-medium">{cliente.nombre}</TableCell>
-        //                 <TableCell>{cliente.domicilio}</TableCell>
-        //                 <TableCell className="text-right"> {cliente.idLocalidad}</TableCell>
-        //                 <TableCell className="text-right">{cliente.idPromotor}</TableCell>
-        //                 <TableCell className="text-right">$ {cliente.saldo}</TableCell>
-        //             </TableRow>
-        //         ))}
-        //     </TableBody>
-        //     <TableFooter>
-        //         <TableRow>
-        //             <TableCell colSpan={4}>Total</TableCell>
-        //             <TableCell className="text-right">
-        //                 $ {clientes?.reduce((acc, el) => acc + el.saldo, 0).toFixed(2)}
-        //             </TableCell>
-        //         </TableRow>
-        //     </TableFooter>
-        // </Table>
-    );
+    return <DataTable columns={columns} data={clientes!} />;
 }
