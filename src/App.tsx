d0e1070fe645bd95fe225c0319e4ce3874214a1f
SantiCombina <santicombina@gmail.com>
2024-06-25 21:14:03 -0300

    Add sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import {Link, Route, Routes} from "react-router-dom";
+import {Toaster} from "sonner";
 
 import {Layout} from "./components/layout/layout";
 import {Clients} from "./views/clients";
+import {Promoters} from "./views/promoters";
+import {Locations} from "./views/locations";
 
 function App() {
     return (
@@ -10,8 +13,11 @@ function App() {
                 <Route element={<Layout />}>
                     <Route element={<Link to="/clientes">Clientes</Link>} path="/" />
                     <Route element={<Clients />} path="/clientes" />
+                    <Route element={<Promoters />} path="/promotores" />
+                    <Route element={<Locations />} path="/localidades" />
                 </Route>
             </Routes>
+            <Toaster richColors />
         </>
     );
 }
